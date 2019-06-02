ac55d585ac7a31ad94adff41a7e20f6332bb17a6
Justas Dabrila <justasdabrila@gmail.com>
2019-06-02 19:39:12 +0000

    add some good stuff

--- r20esdiscordbot/Program.cs
+++ r20esdiscordbot/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Net.Queue;
 using Discord.WebSocket;
+using Microsoft.CSharp.RuntimeBinder;
 using Newtonsoft.Json;
 
 namespace r20esdiscordbot
@@ -12,10 +16,15 @@ namespace r20esdiscordbot
     class BotConfig
     {
         public string Token { get; set; }
+        public ulong Server { get; set; }
+        public ulong IJustJoinedTheServerRole { get; set; }
+        public ulong IssueChannel { get; set; }
     }
     
     class Program
     {
+        private static BotConfig config;
+        
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
@@ -23,9 +32,8 @@ namespace r20esdiscordbot
         {
             try
             {
-                //const ulong channelId = 327823001112412163UL;
-                const ulong channelId = 495907473279156224UL;
-                var targetChannel = (ITextChannel)discord.GetChannel(channelId);
+                //const ulong channelId = 495907473279156224UL;
+                var targetChannel = (ITextChannel)discord.GetChannel(config.IssueChannel);
                 
                 var builder = new StringBuilder();
                 
@@ -45,11 +53,10 @@ namespace r20esdiscordbot
                 Console.WriteLine($"FATAL: exception during SendMessageToIssueChannelAndPingPeople ${ex}.");
             }
         } 
-        
 
         public async Task MainAsync()
         {
-            var config = JsonConvert.DeserializeObject<BotConfig>(File.ReadAllText("config.json"));
+            config = JsonConvert.DeserializeObject<BotConfig>(File.ReadAllText("config.json"));
             
             var client = new DiscordSocketClient();
 
@@ -64,15 +71,68 @@ namespace r20esdiscordbot
                 return Task.CompletedTask;
             };
 
-            client.MessageReceived += async (message) =>
+            client.UserJoined += async (message) =>
             {
-                var trimmed = message.Content.Trim();
+                if (message.Id == client.CurrentUser.Id) return;
+                if (message.IsBot) return;
+                if (message.IsWebhook) return;
+                if (message.Guild.Id != config.Server) return;
+                
+                var role = message.Guild.GetRole(config.IJustJoinedTheServerRole);
 
-                // !console
-                // !background-page
-                if (trimmed.StartsWith("!issue"))
+                await message.AddRoleAsync(role);
+            };
+            
+            client.MessageReceived += async (inMessage) =>
+            {
+                if (inMessage.Author.Id == client.CurrentUser.Id) return;
+                if (inMessage.Author.IsBot) return;
+                if (inMessage.Author.IsWebhook) return;
+
+                if (!(inMessage is SocketUserMessage)) return;
+                var message = (SocketUserMessage) inMessage;
+                
+                if(message.Channel is IDMChannel dmChannel)
                 {
-                    const string template = @"Please use the following template to file an issue:
+                    if (message.Content.ToLowerInvariant() == "y")
+                    {
+                        var server = client.GetGuild(config.Server);
+                        var userInServer = server.GetUser(message.Author.Id);
+                        var role = server.GetRole(config.IJustJoinedTheServerRole);
+                        if (userInServer == null)
+                        {
+                            await message.Channel.SendMessageAsync("userInServer: server is null. Please contact stormy#0427 to get access to the server.");
+                        }
+                        else if (role == null)
+                        {
+                            await message.Channel.SendMessageAsync("role: server is null. Please contact stormy#0427 to get access to the server.");
+                        }
+                        else if (server == null)
+                        {
+                            await message.Channel.SendMessageAsync("bug: server is null. Please contact stormy#0427 to get access to the server.");
+                        }
+                        else
+                        {
+                            await userInServer.RemoveRoleAsync(role);
+                            await message.Channel.SendMessageAsync("You can now access the rest of the server!");
+                        }
+                    }
+                    else
+                    {
+                        await message.Channel.SendMessageAsync("Nothing interesting happens.");
+                    }
+                }
+                else if(message.Channel is IGuildChannel serverChannel)
+                {
+                    if (serverChannel.Guild.Id != config.Server) return;
+
+                    var trimmed = message.Content.Trim();
+
+                    // !console
+                    // !background-page
+                    if (trimmed.StartsWith("!issue"))
+                    {
+                        const string template = @"Please use the following template to file an issue:
 ```
 ISSUE: <What is the issue? Explain as detailed as you can, this is also the perfect place for screenshots of the issue if applicable>
 STEPS: <What did I do, or didn't do to get to this issue?>
@@ -80,22 +140,22 @@ NOTES: <Any extra information you think could be useful?>
 BROWSER: <What browser (w/ version) do you use? What version of the extension do you use? What other extensions do you use (screenshots preferred)?>
 ```
 ";
-                    await SendMessageToIssueChannelAndPingPeople(template, client, message.MentionedUsers);
-                }
+                        await SendMessageToIssueChannelAndPingPeople(template, client, message.MentionedUsers);
+                    }
 
-                if (trimmed.StartsWith("!console"))
-                {
-                    const string template = @"Accessing the web console:
+                    if (trimmed.StartsWith("!console"))
+                    {
+                        const string template = @"Accessing the web console:
 :one: Press F12.
 :two: In the top bar of the new window, press console.";
-                    
-                    await SendMessageToIssueChannelAndPingPeople(template, client, message.MentionedUsers);
-                    
-                }
 
-                if (trimmed.StartsWith("!background-page"))
-                {
-                    const string template = @"Accessing the background page:
+                        await SendMessageToIssueChannelAndPingPeople(template, client, message.MentionedUsers);
+
+                    }
+
+                    if (trimmed.StartsWith("!background-page"))
+                    {
+                        const string template = @"Accessing the background page:
 **Chrome**
     :one: Navigate to `chrome://extensions` in the URL bar.
     :two: Turn on developer mode in the top right.
@@ -109,9 +169,10 @@ BROWSER: <What browser (w/ version) do you use? What version of the extension do
     :four: Accept the debugging connection (nobody's taking over your browser, no worries.)
     :five: In the top bar of the new window, press console.
 ";
-                    
-                    await SendMessageToIssueChannelAndPingPeople(template, client, message.MentionedUsers);
-                    
+
+                        await SendMessageToIssueChannelAndPingPeople(template, client, message.MentionedUsers);
+
+                    }
                 }
             };
 
